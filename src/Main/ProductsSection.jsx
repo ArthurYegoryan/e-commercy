f8aee09d6d5ad84fc57bf259647ae4084b3b8fef
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2023-12-08 11:38:28 +0400

    feat(MakeProductCall.jsx): add functional for making call in new file

--- src/Main/ProductsSection.jsx
+++ src/Main/ProductsSection.jsx
@@ -3,11 +3,13 @@ import GetProducts from "../services/GetProducts.jsx";
 const ProoductsSection = () => {
     const products = GetProducts();
 
-    return (
-        <section>
-            {/* Barlus Hayer */}
-        </section>
-    );
+    setTimeout(() => {
+        return (
+            <section>
+                {products.map((product) => <p key={product.id}>{product.title}</p>)}
+            </section>
+        );
+    }, 1000);
 };
 
 export default ProoductsSection;
--- src/services/GetProducts.jsx
+++ src/services/GetProducts.jsx
@@ -1,21 +1,15 @@
 import { useEffect, useState } from "react";
+import MakeProductsCall from "./MakeProductsCall";
 
 const GetProducts = () => {
     const [ data, setData ] = useState(null);
 
     useEffect(() => {
-        // try {
-        //     // const response = await fetch("https://fakestoreapi.com/products");
-        //     // const result = await response.json();
-            
-            
-        // } catch (err) {
-        //     console.error(err.message);
-        // }
-        fetch("https://fakestoreapi.com/products")
-                .then((response) => response.json())
-                .then((data) => setData(data))
-                .catch((error) => console.error(error));
+        MakeProductsCall({setData});
+        // fetch("https://fakestoreapi.com/products")
+        //         .then((response) => response.json())
+        //         .then((data) => setData(data))
+        //         .catch((error) => console.error(error));
     }, []);
 
     return data;
