d31a5d78cc7c3acd491fafc6549c2ab30a17d222
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2023-12-08 15:32:29 +0400

    fix(ProductsSection): fix two times call bug

--- src/Main/ProductsSection.jsx
+++ src/Main/ProductsSection.jsx
@@ -1,25 +1,21 @@
-import { useEffect ,useState } from "react";
+import { useEffect, useState } from "react";
 
 const ProoductsSection = () => {
     const [ data, setData ] = useState(null);
 
     useEffect(() => {
-        // fetch("https://fakestoreapi.com/products")
-        // .then((response) => response.json())
-        // .then((data) => setData(data))
-        // .catch((error) => console.error(error));
-
         const fetchData = async () => {
             const response = await fetch("https://fakestoreapi.com/products");
             const result = await response.json();
-            setData(await result);
-        };
+            setData(result);
+        }
+
         fetchData();
-    });
+    }, []);
 
     return (
         <section>
-            {data.map((product) => <p key={product.id}>{product.title}</p>)}
+            {data && data.map((product) => <p key={product.id}>{product.title}</p>)}
         </section>
     );
 };
